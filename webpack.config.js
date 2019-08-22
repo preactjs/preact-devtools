fd7062d8a611de6ca2713d950801378cb4ea8789
Marvin Hagemeister <hello@marvinh.dev>
2019-08-22 21:19:49 +0200

    Add babel setup for mixed preact transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: {
-		app: "./src/index.ts",
+		app: "./src/index.tsx",
 	},
 	resolve: {
 		extensions: [".tsx", ".ts", ".js", ".json"],
@@ -26,8 +26,69 @@ module.exports = {
 				],
 			},
 			{
+				test: /\.jsx?/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "babel-loader",
+						options: {
+							plugins: [
+								[
+									"@babel/plugin-transform-react-jsx",
+									{
+										pragma: "h",
+										pragmaFrag: "Fragment",
+									},
+								],
+							],
+						},
+					},
+				],
+			},
+			{
 				test: /\.tsx?/,
-				use: ["ts-loader"],
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "babel-loader",
+						options: {
+							plugins: [
+								[
+									"babel-plugin-transform-rename-properties",
+									{
+										rename: {
+											_depth: "__b",
+											_dirty: "__d",
+											_nextState: "__s",
+											_renderCallbacks: "__h",
+											_vnode: "__v",
+											_children: "__k",
+											_dom: "__e",
+											_lastDomChild: "__z",
+											_component: "__c",
+											__html: "__html",
+											_parent: "__p",
+											_pendingError: "__E",
+											_processingException: "__p",
+											_context: "__n",
+											_defaultValue: "__p",
+											_id: "__c",
+											_parentDom: "__P",
+											_root: "__p",
+											_diff: "__b",
+											_commit: "__c",
+											_render: "__r",
+											_hook: "__h",
+											_catchError: "__e",
+											_unmount: "_e",
+										},
+									},
+								],
+							],
+						},
+					},
+					"ts-loader",
+				],
 			},
 		],
 	},
