6c11f7fe0182a12d1c512c959b230eb56ac1144a
Marvin Hagemeister <hello@marvinh.dev>
2019-09-08 14:01:34 +0200

    Fix hover never removed

--- src/view/components/TreeView.tsx
+++ src/view/components/TreeView.tsx
@@ -56,6 +56,7 @@ export function TreeItem(props: { key: any; id: ID }) {
 			class={s.item}
 			onClick={ev => onSelect(id, ev.currentTarget as any)}
 			onMouseEnter={() => onHover(id)}
+			onMouseLeave={() => onHover(null)}
 			data-selected={selected}
 			data-depth={node.depth}
 			style={`padding-left: calc(var(--indent-depth) * ${node.depth})`}
