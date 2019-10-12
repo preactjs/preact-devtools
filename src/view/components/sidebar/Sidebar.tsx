5347abc22c64f62058fd34f28a46405e3a7cf7d3
Marvin Hagemeister <hello@marvinh.dev>
2019-10-12 23:18:18 +0200

    Extract Sidebar top bar

--- src/view/components/sidebar/Sidebar.tsx
+++ src/view/components/sidebar/Sidebar.tsx
@@ -1,13 +1,10 @@
 import { h } from "preact";
 import s from "./Sidebar.css";
-import { Actions } from "../Actions";
-import { useObserver, useStore, useEmitter } from "../../store/react-bindings";
-import { IconBtn } from "../IconBtn";
-import { BugIcon, Refresh } from "../icons";
-import { useCallback } from "preact/hooks";
+import { useEmitter } from "../../store/react-bindings";
 import { PropsPanel } from "./PropsPanel";
 import { PropData } from "../../parseProps";
 import { Collapser } from "../../store/collapser";
+import { SidebarActions } from "./SidebarActions";
 
 const collapseFn = (
 	data: PropData,
@@ -24,36 +21,11 @@ const collapseFn = (
 };
 
 export function Sidebar() {
-	const store = useStore();
 	const emit = useEmitter();
-	const node = useObserver(
-		() => store.nodes.$.get(store.selection.selected.$) || null,
-	);
-	const forceUpdate = useCallback(() => {
-		if (node) emit("force-update", node.id);
-	}, [node]);
-	const log = useCallback(() => {
-		if (node) emit("log", node.id);
-	}, [node]);
 
 	return (
 		<aside class={s.root}>
-			<Actions class={s.actions}>
-				<span class={s.title}>{node ? node.name : "-"}</span>
-
-				<div class={s.iconActions}>
-					{node && node.name[0] === node.name[0].toUpperCase() && (
-						<IconBtn title="Re-render Component" onClick={forceUpdate}>
-							<Refresh />
-						</IconBtn>
-					)}
-					{node && (
-						<IconBtn title="Log internal vnode" onClick={log}>
-							<BugIcon />
-						</IconBtn>
-					)}
-				</div>
-			</Actions>
+			<SidebarActions />
 			<div class={s.body}>
 				<PropsPanel
 					label="Props"
