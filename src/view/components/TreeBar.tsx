c14396eed7cadecb4f505c6a9e72abf165957eb0
Marvin Hagemeister <hello@marvinh.dev>
2019-10-13 08:22:10 +0200

    Change filter icon

--- src/view/components/TreeBar.tsx
+++ src/view/components/TreeBar.tsx
@@ -14,6 +14,7 @@ import {
 	AddCircle,
 	CheckboxChecked,
 	CheckboxUnChecked,
+	FilterList,
 } from "./icons";
 import { useStore, useObserver } from "../store/react-bindings";
 import s from "./TreeBar.css";
@@ -100,7 +101,7 @@ export function TreeBar() {
 						active={filterVisible}
 						onClick={() => setFilterVisible(!filterVisible)}
 					>
-						<Eye />
+						<FilterList />
 					</IconBtn>
 					{filterVisible && <FilterPopup />}
 				</OutsideClick>
--- src/view/components/icons.tsx
+++ src/view/components/icons.tsx
@@ -160,6 +160,16 @@ export function Search({ size = "s" }: Props) {
 	);
 }
 
+export function FilterList({ size = "s" }: Props) {
+	return createSvgIcon(
+		size,
+		<path
+			d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z"
+			fill="currentColor"
+		/>,
+	);
+}
+
 export function CheckboxChecked({ size = "s" }: Props) {
 	return createSvgIcon(
 		size,
