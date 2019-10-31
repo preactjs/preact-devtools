d5901abad6a84e12b8aa884901b7ed3dbc32b3e7
Marvin Hagemeister <hello@marvinh.dev>
2019-10-31 22:45:14 +0100

    Rewrite layout to use CSS Grid

--- src/view/components/sidebar/Sidebar.tsx
+++ src/view/components/sidebar/Sidebar.tsx
@@ -1,10 +1,9 @@
-import { h } from "preact";
+import { h, Fragment } from "preact";
 import s from "./Sidebar.css";
 import { useEmitter } from "../../store/react-bindings";
 import { PropsPanel } from "./PropsPanel";
 import { PropData } from "./parseProps";
 import { Collapser } from "../../store/collapser";
-import { SidebarActions } from "./SidebarActions";
 import { serializeProps } from "./serializeProps";
 import { useCallback } from "preact/hooks";
 
@@ -28,46 +27,41 @@ export function Sidebar() {
 	const onCopy = useCallback((v: any) => emit("copy", serializeProps(v)), []);
 
 	return (
-		<aside class={s.root}>
-			<SidebarActions />
-			<div class={s.body}>
-				<PropsPanel
-					label="Props"
-					getData={d => d.props}
-					checkEditable={data => data.canEditProps}
-					transform={collapseFn}
-					onChange={(id, path, value) =>
-						emit("update-prop", { id, path, value })
-					}
-					onCopy={onCopy}
-					canAddNew
-				/>
-				<PropsPanel
-					label="State"
-					isOptional
-					getData={d => d.state}
-					checkEditable={data => data.canEditState}
-					transform={collapseFn}
-					onChange={(id, path, value) =>
-						emit("update-state", { id, path, value })
-					}
-					onCopy={onCopy}
-				/>
-				<PropsPanel
-					label="Context"
-					isOptional
-					getData={d => d.context}
-					checkEditable={() => true}
-					transform={collapseFn}
-					onChange={(id, path, value) =>
-						emit("update-context", { id, path, value })
-					}
-					onCopy={onCopy}
-				/>
-				{/* {inspect != null && inspect.hooks && (
+		<Fragment>
+			<PropsPanel
+				label="Props"
+				getData={d => d.props}
+				checkEditable={data => data.canEditProps}
+				transform={collapseFn}
+				onChange={(id, path, value) => emit("update-prop", { id, path, value })}
+				onCopy={onCopy}
+				canAddNew
+			/>
+			<PropsPanel
+				label="State"
+				isOptional
+				getData={d => d.state}
+				checkEditable={data => data.canEditState}
+				transform={collapseFn}
+				onChange={(id, path, value) =>
+					emit("update-state", { id, path, value })
+				}
+				onCopy={onCopy}
+			/>
+			<PropsPanel
+				label="Context"
+				isOptional
+				getData={d => d.context}
+				checkEditable={() => true}
+				transform={collapseFn}
+				onChange={(id, path, value) =>
+					emit("update-context", { id, path, value })
+				}
+				onCopy={onCopy}
+			/>
+			{/* {inspect != null && inspect.hooks && (
 					<SidebarPanel title="hooks" empty="None"></SidebarPanel>
 				)} */}
-			</div>
-		</aside>
+		</Fragment>
 	);
 }
