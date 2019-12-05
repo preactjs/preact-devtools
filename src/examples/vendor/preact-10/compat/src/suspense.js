3558fd96e7d4d2c075db629d0d45fc0b6a337afc
Marvin Hagemeister <hello@marvinh.dev>
2019-12-05 14:35:05 +0100

    Vendor Preact for demo pages

---
+++ src/examples/vendor/preact-10/compat/src/suspense.js
@@ -0,0 +1,132 @@
+import {
+	Component,
+	createElement,
+	_unmount as unmount,
+	options,
+	cloneElement
+} from 'preact';
+import { removeNode } from '../../src/util';
+
+const oldCatchError = options._catchError;
+options._catchError = function(error, newVNode, oldVNode) {
+	if (error.then && oldVNode) {
+		/** @type {import('./internal').Component} */
+		let component;
+		let vnode = newVNode;
+
+		for (; (vnode = vnode._parent); ) {
+			if ((component = vnode._component) && component._childDidSuspend) {
+				if (oldVNode) {
+					newVNode._dom = oldVNode._dom;
+					newVNode._children = oldVNode._children;
+				}
+
+				component._childDidSuspend(error);
+				return; // Don't call oldCatchError if we found a Suspense
+			}
+		}
+	}
+
+	oldCatchError(error, newVNode, oldVNode);
+};
+
+function detachDom(children) {
+	for (let i = 0; i < children.length; i++) {
+		let child = children[i];
+		if (child != null) {
+			if (typeof child.type !== 'function' && child._dom) {
+				removeNode(child._dom);
+			} else if (child._children) {
+				detachDom(child._children);
+			}
+		}
+	}
+}
+
+// having custom inheritance instead of a class here saves a lot of bytes
+export function Suspense(props) {
+	// we do not call super here to golf some bytes...
+	this._suspensions = [];
+	this._fallback = props.fallback;
+}
+
+// Things we do here to save some bytes but are not proper JS inheritance:
+// - call `new Component()` as the prototype
+// - do not set `Suspense.prototype.constructor` to `Suspense`
+Suspense.prototype = new Component();
+
+/**
+ * @param {Promise} promise The thrown promise
+ */
+Suspense.prototype._childDidSuspend = function(promise) {
+	/** @type {import('./internal').SuspenseComponent} */
+	const c = this;
+	c._suspensions.push(promise);
+
+	const onSuspensionComplete = () => {
+		// From https://twitter.com/Rich_Harris/status/1125850391155965952
+		c._suspensions[c._suspensions.indexOf(promise)] =
+			c._suspensions[c._suspensions.length - 1];
+		c._suspensions.pop();
+
+		if (c._suspensions.length == 0) {
+			// If fallback is null, don't try to unmount it
+			// `unmount` expects a real VNode, not null values
+			if (c._fallback) {
+				// Unmount current children (should be fallback)
+				unmount(c._fallback);
+			}
+			c._vnode._dom = null;
+
+			c._vnode._children = c.state._parkedChildren;
+			c.setState({ _parkedChildren: null });
+		}
+	};
+
+	if (c.state._parkedChildren == null) {
+		c._fallback = c._fallback && cloneElement(c._fallback);
+		c.setState({ _parkedChildren: c._vnode._children });
+		detachDom(c._vnode._children);
+		c._vnode._children = [];
+	}
+
+	promise.then(onSuspensionComplete, onSuspensionComplete);
+};
+
+Suspense.prototype.render = function(props, state) {
+	return state._parkedChildren ? this._fallback : props.children;
+};
+
+export function lazy(loader) {
+	let prom;
+	let component;
+	let error;
+
+	function Lazy(props) {
+		if (!prom) {
+			prom = loader();
+			prom.then(
+				exports => {
+					component = exports.default;
+				},
+				e => {
+					error = e;
+				}
+			);
+		}
+
+		if (error) {
+			throw error;
+		}
+
+		if (!component) {
+			throw prom;
+		}
+
+		return createElement(component, props);
+	}
+
+	Lazy.displayName = 'Lazy';
+	Lazy._forwarded = true;
+	return Lazy;
+}
---
+++ src/examples/vendor/preact-10/src/render.js
@@ -0,0 +1,52 @@
+import { EMPTY_OBJ, EMPTY_ARR } from './constants';
+import { commitRoot, diff } from './diff/index';
+import { createElement, Fragment } from './create-element';
+import options from './options';
+
+const IS_HYDRATE = EMPTY_OBJ;
+
+/**
+ * Render a Preact virtual node into a DOM element
+ * @param {import('./index').ComponentChild} vnode The virtual node to render
+ * @param {import('./internal').PreactElement} parentDom The DOM element to
+ * render into
+ * @param {Element | Text} [replaceNode] Attempt to re-use an
+ * existing DOM tree rooted at `replaceNode`
+ */
+export function render(vnode, parentDom, replaceNode) {
+	if (options._root) options._root(vnode, parentDom);
+
+	let isHydrating = replaceNode === IS_HYDRATE;
+	let oldVNode = isHydrating
+		? null
+		: (replaceNode && replaceNode._children) || parentDom._children;
+	vnode = createElement(Fragment, null, [vnode]);
+
+	let commitQueue = [];
+	diff(
+		parentDom,
+		((isHydrating ? parentDom : replaceNode || parentDom)._children = vnode),
+		oldVNode || EMPTY_OBJ,
+		EMPTY_OBJ,
+		parentDom.ownerSVGElement !== undefined,
+		replaceNode && !isHydrating
+			? [replaceNode]
+			: oldVNode
+			? null
+			: EMPTY_ARR.slice.call(parentDom.childNodes),
+		commitQueue,
+		replaceNode || EMPTY_OBJ,
+		isHydrating
+	);
+	commitRoot(commitQueue, vnode);
+}
+
+/**
+ * Update an existing DOM element with data from a Preact virtual node
+ * @param {import('./index').ComponentChild} vnode The virtual node to render
+ * @param {import('./internal').PreactElement} parentDom The DOM element to
+ * update
+ */
+export function hydrate(vnode, parentDom) {
+	render(vnode, parentDom, IS_HYDRATE);
+}
