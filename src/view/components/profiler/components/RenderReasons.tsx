e7c694c601e705368a5d3d69468ff1b50d857c4d
Marvin Hagemeister <hello@marvinh.dev>
2020-01-10 23:04:08 +0100

    Fix wrong message for rendered reasons

--- src/view/components/profiler/components/RenderReasons.tsx
+++ src/view/components/profiler/components/RenderReasons.tsx
@@ -6,12 +6,29 @@ export function RenderReasons() {
 	const store = useStore();
 	const isRecording = useObserver(() => store.profiler.isRecording.$);
 	const commits = useObserver(() => store.profiler.commits.$);
+	const commit = useObserver(() => store.profiler.activeCommit.$);
+	const selected = useObserver(() => store.profiler.selectedNode.$);
 
 	if (commits.length === 0 || isRecording) {
 		return null;
 	}
 
+	let rendered = false;
+	if (commit) {
+		const root = commit.nodes.get(commit.commitRootId);
+		if (
+			root &&
+			selected &&
+			selected.startTime >= root.startTime &&
+			selected.endTime <= root.endTime
+		) {
+			rendered = true;
+		}
+	}
+
 	return (
-		<SidebarPanel title="Render reasons" empty="Did not render"></SidebarPanel>
+		<SidebarPanel title="Render reasons" empty="Did not render">
+			{rendered ? "-" : null}
+		</SidebarPanel>
 	);
 }
