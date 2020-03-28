b2f12514ba467a636b71c9f3acc7471b482ead81
Marvin Hagemeister <hello@marvinh.dev>
2020-03-28 21:46:08 +0100

    Fix inspect picker triggering app events

---
+++ test-e2e/tests/inspect-click.test.ts
@@ -0,0 +1,22 @@
+import { newTestPage, click, getLog, getText } from "../test-utils";
+import { expect } from "chai";
+import { closePage } from "pintf/browser_utils";
+
+export const description = "Don't trigger events on click during inspection";
+
+export async function run(config: any) {
+	const { page, devtools } = await newTestPage(config, "counter");
+
+	const target = '[data-testid="result"]';
+	expect(await getText(page, target)).to.equal("Counter: 0");
+
+	const inspect = '[data-testid="inspect-btn"]';
+	await click(devtools, inspect);
+
+	await page.click("button");
+
+	const text = await getText(page, target);
+	expect(text).to.equal("Counter: 0");
+
+	await closePage(page);
+}
