5ac25963ff164036edb1ae7ec890234f1b00fccd
Marvin Hagemeister <hello@marvinh.dev>
2020-03-28 17:16:01 +0100

    Fix highlight flickering

---
+++ test-e2e/tests/highlighter.test.ts
@@ -0,0 +1,20 @@
+import { newTestPage } from "../test-utils";
+import { expect } from "chai";
+import { closePage } from "pintf/browser_utils";
+import { wait } from "pintf/utils";
+
+export const description = "Mirror component state to the devtools";
+
+export async function run(config: any) {
+	const { page, devtools } = await newTestPage(config, "counter");
+
+	await devtools.hover('[data-testid="tree-item"][data-name="Counter"]');
+
+	// Wait for possible flickering to occur
+	await wait(1000);
+
+	const log = (await page.evaluate(() => (window as any).log)) as any[];
+	expect(log.filter(x => x.type === "highlight").length).to.equal(1);
+
+	await closePage(page);
+}
