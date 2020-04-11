48d24f0789316f0f0906d76ee59718a18a1cf9fd
Marvin Hagemeister <hello@marvinh.dev>
2020-04-11 09:10:22 +0200

    Only collect set marks when supported

--- src/adapter/10/marks.ts
+++ src/adapter/10/marks.ts
@@ -4,19 +4,27 @@
 
 const markName = (s: string) => `⚛ ${s}`;
 
+const supportsPerformance =
+	globalThis.performance &&
+	typeof globalThis.performance.getEntriesByName === "function";
+
 export function recordMark(s: string) {
-	performance.mark(markName(s));
+	if (supportsPerformance) {
+		performance.mark(markName(s));
+	}
 }
 
 export function endMark(nodeName: string) {
-	const name = markName(nodeName);
-	const start = `${name}_diff`;
-	const end = `${name}_diffed`;
-	if (performance.getEntriesByName(start).length > 0) {
-		performance.mark(end);
-		performance.measure(name, start, end);
+	if (supportsPerformance) {
+		const name = markName(nodeName);
+		const start = `${name}_diff`;
+		const end = `${name}_diffed`;
+		if (performance.getEntriesByName(start).length > 0) {
+			performance.mark(end);
+			performance.measure(name, start, end);
+		}
+		performance.clearMarks(start);
+		performance.clearMarks(end);
+		performance.clearMeasures(name);
 	}
-	performance.clearMarks(start);
-	performance.clearMarks(end);
-	performance.clearMeasures(name);
 }
