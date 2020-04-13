9d851a331f7987fb4d9269cebda37b8b52bd45c2
Marvin Hagemeister <hello@marvinh.dev>
2020-04-13 09:53:24 +0200

    Add log vnode to profiler view

--- src/view/components/profiler/components/SidebarHeader.tsx
+++ src/view/components/profiler/components/SidebarHeader.tsx
@@ -1,16 +1,34 @@
 import { h } from "preact";
 import { Actions } from "../../Actions";
 import s from "../../sidebar/Sidebar.css";
-import { useStore, useObserver } from "../../../store/react-bindings";
+import {
+	useStore,
+	useObserver,
+	useEmitter,
+} from "../../../store/react-bindings";
 import { ComponentName } from "../../ComponentName";
+import { useCallback } from "preact/hooks";
+import { IconBtn } from "../../IconBtn";
+import { BugIcon } from "../../icons";
 
 export function SidebarHeader() {
 	const store = useStore();
 	const selected = useObserver(() => store.profiler.selectedNode.$);
+	const emit = useEmitter();
+	const log = useCallback(() => {
+		if (selected) emit("log", { id: selected.id, children: selected.children });
+	}, [selected]);
 
 	return (
 		<Actions class={s.actions}>
 			<ComponentName>{selected && selected.name}</ComponentName>
+			<div class={s.iconActions}>
+				{selected && (
+					<IconBtn title="Log internal vnode" onClick={log}>
+						<BugIcon />
+					</IconBtn>
+				)}
+			</div>
 		</Actions>
 	);
 }
