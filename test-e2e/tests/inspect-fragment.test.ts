c07b6b24f725ee4919bfae18825acb41f8995634
Marvin Hagemeister <hello@marvinh.dev>
2020-04-15 23:00:26 +0200

    Add test case for highlighting Fragments

---
+++ test-e2e/tests/inspect-fragment.test.ts
@@ -0,0 +1,34 @@
+import { newTestPage, getSize } from "../test-utils";
+import { expect } from "chai";
+import { closePage, waitForTestId } from "pintf/browser_utils";
+
+export const description = "Highlighting combined DOM tree of a Fragment";
+
+export async function run(config: any) {
+	const { page, devtools } = await newTestPage(config, "highlight-fragment");
+
+	// 1st test
+	let size = await getSize(page, '[data-testid="test1"]');
+	await devtools.waitForSelector('[data-testid="tree-item"]', {
+		timeout: 2000,
+	});
+
+	const items = await devtools.$$('[data-testid="tree-item"]');
+	await items[0]!.click();
+	await waitForTestId(page, "highlight", { timeout: 2000 });
+
+	let highlight = await getSize(page, '[data-testid="highlight"]');
+	expect(size.width).to.equal(highlight.width);
+	expect(size.height).to.equal(highlight.height);
+
+	// 2nd test
+	size = await getSize(page, '[data-testid="test2"]');
+	await items[1]!.click();
+	await waitForTestId(page, "highlight", { timeout: 2000 });
+
+	highlight = await getSize(page, '[data-testid="highlight"]');
+	expect(size.width).to.equal(highlight.width);
+	expect(size.height).to.equal(highlight.height);
+
+	await closePage(page);
+}
