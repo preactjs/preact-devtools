05bbdadc82a5296e3d8b35bbcc418ffff496f8a6
Marvin Hagemeister <hello@marvinh.dev>
2020-04-16 23:26:10 +0200

    Limit key length display

---
+++ src/view/components/elements/TreeView.test.tsx
@@ -0,0 +1,41 @@
+import { h } from "preact";
+import { render } from "@testing-library/preact";
+import { TreeItem } from "./TreeView";
+import { expect } from "chai";
+import { AppCtx } from "../../store/react-bindings";
+import { createStore } from "../../store";
+import { DevNodeType } from "../../store/types";
+
+describe("TreeItem", () => {
+	it("should limit key length to 15", () => {
+		const store = createStore();
+		store.nodes.$.set(1, {
+			children: [],
+			depth: 1,
+			endTime: 0,
+			key: "abcdefghijklmnopqrstuvxyz",
+			id: 1,
+			name: "foo",
+			parent: -1,
+			startTime: 0,
+			treeEndTime: 0,
+			treeStartTime: 0,
+			type: DevNodeType.ClassComponent,
+		});
+		const { container, rerender } = render(
+			<AppCtx.Provider value={store}>
+				<TreeItem id={1} key="" />,
+			</AppCtx.Provider>,
+		);
+		expect(container.textContent).to.equal('foo key="abcdefghijklmno…",');
+
+		store.nodes.$.get(1)!.key = "foobar";
+		rerender(
+			<AppCtx.Provider value={store}>
+				<TreeItem id={1} key="" />,
+			</AppCtx.Provider>,
+		);
+
+		expect(container.textContent).to.equal('foo key="foobar",');
+	});
+});
