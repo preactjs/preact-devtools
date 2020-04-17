30f728f8a4bf396d003c1200947e78eb74d4ae60
Marvin Hagemeister <hello@marvinh.dev>
2020-04-17 18:53:48 +0200

    Fix linting errors

--- src/adapter/10/renderer/inspectVNode.ts
+++ src/adapter/10/renderer/inspectVNode.ts
@@ -12,50 +12,9 @@ import { VNode, Component, Options } from "preact";
 import { parseStackTrace, StackFrame } from "errorstacks";
 import { HookType } from "../../../constants";
 import { debug } from "../../../debug";
-import { ObjPath } from "../../../view/components/sidebar/inspect/ElementProps";
 
 function serialize(config: RendererConfig10, data: object | null) {
 	return jsonify(data, node => serializeVNode(node, config), new Set());
-}
-
-/**
- * Serialize all properties/attributes of a `VNode` like `props`, `context`,
- * `hooks`, and other data. This data will be requested when the user selects a
- * `VNode` in the devtools. It returning information will be displayed in the
- * sidebar.
- */
-export function inspectVNode(
-	ids: IdMappingState,
-	config: RendererConfig10,
-	options: Options,
-	id: ID,
-) {
-	const vnode = getVNodeById(ids, id);
-	if (!vnode) return null;
-
-	const c = getComponent(vnode);
-	const hasState =
-		typeof vnode.type === "function" &&
-		c != null &&
-		Object.keys(c.state).length > 0;
-
-	const hasHooks = c != null && getComponentHooks(c) != null;
-	const hooks = hasHooks ? inspectHooks(config, options, vnode) : null;
-	const context = c != null ? serialize(config, cleanContext(c.context)) : null;
-	const props =
-		vnode.type !== null ? serialize(config, cleanProps(vnode.props)) : null;
-	const state = hasState ? serialize(config, c!.state) : null;
-
-	return {
-		context,
-		hooks,
-		id,
-		name: getDisplayName(vnode, config),
-		props,
-		state,
-		// TODO: We're not using this information anywhere yet
-		type: getDevtoolsType(vnode),
-	};
 }
 
 /**
@@ -191,7 +150,7 @@ export interface HookItem {
 }
 
 const nativeName = /^\d+__\S+$/;
-export function getHookFromPath(objPath: ObjPath) {
+export function getHookFromPath(objPath: any) {
 	for (let i = objPath.length - 1; i >= 0; i--) {
 		if (nativeName.test("" + objPath[i])) {
 			return objPath.slice(0, i);
@@ -199,4 +158,44 @@ export function getHookFromPath(objPath: ObjPath) {
 	}
 
 	return objPath;
+}
+
+/**
+ * Serialize all properties/attributes of a `VNode` like `props`, `context`,
+ * `hooks`, and other data. This data will be requested when the user selects a
+ * `VNode` in the devtools. It returning information will be displayed in the
+ * sidebar.
+ */
+export function inspectVNode(
+	ids: IdMappingState,
+	config: RendererConfig10,
+	options: Options,
+	id: ID,
+) {
+	const vnode = getVNodeById(ids, id);
+	if (!vnode) return null;
+
+	const c = getComponent(vnode);
+	const hasState =
+		typeof vnode.type === "function" &&
+		c != null &&
+		Object.keys(c.state).length > 0;
+
+	const hasHooks = c != null && getComponentHooks(c) != null;
+	const hooks = hasHooks ? inspectHooks(config, options, vnode) : null;
+	const context = c != null ? serialize(config, cleanContext(c.context)) : null;
+	const props =
+		vnode.type !== null ? serialize(config, cleanProps(vnode.props)) : null;
+	const state = hasState ? serialize(config, c!.state) : null;
+
+	return {
+		context,
+		hooks,
+		id,
+		name: getDisplayName(vnode, config),
+		props,
+		state,
+		// TODO: We're not using this information anywhere yet
+		type: getDevtoolsType(vnode),
+	};
 }
