813442346354e366454e11200544b5fc8b5e6302
Marvin Hagemeister <hello@marvinh.dev>
2020-05-19 20:52:01 +0200

    Fix profiler nodes getting lost due to mutations

--- src/adapter/events/legacy/operationsV1.ts
+++ src/adapter/events/legacy/operationsV1.ts
@@ -1,8 +1,11 @@
 import { MsgTypes } from "../events";
 import { parseTable } from "../../string-table";
 import { Store } from "../../../view/store/types";
+import { deepClone } from "../../../view/components/profiler/flamegraph/modes/adjustNodesToRight";
 
 export function applyOperationsV1(store: Store, data: number[]) {
+	const nodes = new Map(store.nodes.$);
+
 	let i = data[1] + 1;
 	const strings = parseTable(data.slice(1, i + 1));
 
@@ -23,24 +26,26 @@ export function applyOperationsV1(store: Store, data: number[]) {
 				const key = data[i + 6] > 0 ? strings[data[i + 6] - 1] : "";
 				let parentId = data[i + 3];
 
-				if (!store.nodes.$.has(id)) {
+				if (!nodes.has(id)) {
 					// Roots have their own id as parentId
 					if (id !== parentId) {
-						const parent = store.nodes.$.get(parentId);
+						const parent = nodes.get(parentId);
 						if (!parent) {
 							// throw new Error(`Parent node ${parentId} not found in store.`);
 							// eslint-disable-next-line no-console
 							console.warn(`Parent node ${parentId} not found in store.`);
 							parentId = -1;
 						} else {
-							parent.children.push(id);
+							const clone = deepClone(parent);
+							nodes.set(clone.id, clone);
+							clone.children.push(id);
 						}
 					}
 
-					const parent = store.nodes.$.get(parentId)!;
+					const parent = nodes.get(parentId)!;
 					const depth = parent ? parent.depth + 1 : 1;
 
-					store.nodes.$.set(id, {
+					nodes.set(id, {
 						children: [],
 						depth,
 						id,
@@ -72,30 +77,34 @@ export function applyOperationsV1(store: Store, data: number[]) {
 				const len = i + unmounts;
 				for (; i < len; i++) {
 					const nodeId = data[i];
-					const node = store.nodes.$.get(nodeId);
+					const node = nodes.get(nodeId);
 					if (node) {
 						// Remove node from parent children array
 						const parentId = node.parent;
-						const parent = store.nodes.$.get(parentId);
+						const parent = nodes.get(parentId);
 						if (parent) {
 							const idx = parent.children.indexOf(nodeId);
-							if (idx > -1) parent.children.splice(idx, 1);
+							if (idx > -1) {
+								const clone = deepClone(parent);
+								nodes.set(clone.id, clone);
+								clone.children.splice(idx, 1);
+							}
 						}
 
 						const stack = node.children;
 						while (stack.length > 0) {
 							const childId = stack.pop();
 							if (childId != null) {
-								const childNode = store.nodes.$.get(childId);
+								const childNode = nodes.get(childId);
 								if (childNode) {
 									stack.push(...childNode.children);
-									store.nodes.$.delete(childId);
+									nodes.delete(childId);
 								}
 							}
 						}
 
 						// Remove node from store
-						store.nodes.$.delete(nodeId);
+						nodes.delete(nodeId);
 					}
 				}
 
@@ -106,9 +115,11 @@ export function applyOperationsV1(store: Store, data: number[]) {
 			case MsgTypes.REORDER_CHILDREN: {
 				const parentId = data[i + 1];
 				const count = data[i + 2];
-				const parent = store.nodes.$.get(parentId);
+				const parent = nodes.get(parentId);
 				if (parent) {
-					parent.children = data.slice(i + 3, i + 3 + count);
+					const clone = deepClone(parent);
+					nodes.set(clone.id, clone);
+					clone.children = data.slice(i + 3, i + 3 + count);
 				}
 				i = i + 3 + count;
 				break;
@@ -117,5 +128,5 @@ export function applyOperationsV1(store: Store, data: number[]) {
 	}
 
 	store.roots.update();
-	store.nodes.update();
+	store.nodes.$ = nodes;
 }
