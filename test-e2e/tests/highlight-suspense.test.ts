946e12c67ce5f643b517045e69d51af0c98c03ae
Marvin Hagemeister <hello@marvinh.dev>
2020-09-22 21:35:39 +0200

    Update Suspense highlight test

--- test-e2e/tests/highlight-suspense.test.ts
+++ test-e2e/tests/highlight-suspense.test.ts
@@ -1,29 +1,24 @@
 import { newTestPage, getSize } from "../test-utils";
 import { expect } from "chai";
 import { wait } from "pentf/utils";
-
+import { waitForTestId } from "pentf/browser_utils";
 import type { Page } from "puppeteer";
-
-export const description = "Display and highlighting of Suspense in devtools";
 
 function getHighlightSize(page: Page): unknown {
 	return getSize(page, "#preact-devtools-highlighter > div");
 }
 
-export async function run(config: any): Promise<void> {
+export const description = "Highlight Suspense nodes without crashing";
+export async function run(config: any) {
 	const { page, devtools } = await newTestPage(config, "suspense");
 
-	// TODO: would expect tree-item list to be:
-	//   ["Block", "Shortly", "Suspense", "Delayed", "Block"]
-	// but is actually:
-	//   ["Block", "Shortly", "Suspense", "m"]
+	await waitForTestId(devtools, "tree-item");
 
 	await devtools.hover('[data-testid="tree-item"][data-name="Suspense"]');
 	// Wait for possible flickering to occur
 	await wait(1000);
-	const sizeOnPage = await getSize(page, '[data-testid="container"]');
+
+	const sizeOnPage = await getSize(page, '[data-testid="delayed"]');
 	const sizeOfHighlight = await getHighlightSize(page);
 	expect(sizeOfHighlight).to.eql(sizeOnPage);
-
-	// TODO: expect size of delayed child to be correctly shown
 }
