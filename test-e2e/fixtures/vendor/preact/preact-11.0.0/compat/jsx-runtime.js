7ec11cd0ed5678d0e63b351004b63cf49ed2db2d
Marvin Hagemeister <hello@marvinh.dev>
2022-02-20 15:06:58 +0100

    Drop vendored preact 11 release

--- test-e2e/fixtures/vendor/preact/preact-11.0.0/compat/jsx-runtime.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('preact/jsx-runtime');
--- test-e2e/fixtures/vendor/preact/preact-11.0.0/jsx-runtime/dist/jsxRuntime.mjs
+++
@@ -1,2 +0,0 @@
-import{options as r}from"preact";export{Fragment}from"preact";let t=0;function e(e,o,s,f,n){let p={};for(let r in o)"ref"!=r&&(p[r]=o[r]);const c={type:e,props:p,key:s,ref:o&&o.ref,constructor:void 0,__v:--t,__source:f,__self:n};let i,a;if("function"==typeof e&&(i=e.defaultProps))for(a in i)void 0===p[a]&&(p[a]=i[a]);return r.vnode&&r.vnode(c),c}export{e as jsx,e as jsxDEV,e as jsxs};
-//# sourceMappingURL=jsxRuntime.mjs.map
