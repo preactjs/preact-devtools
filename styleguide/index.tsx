070394c0b798378a11ee6eac3b97622990806f4d
Marvin Hagemeister <hello@marvinh.dev>
2022-05-18 07:12:28 +0200

    Remove unused styleguide

--- styleguide/index.tsx
+++
@@ -1,8 +0,0 @@
-import { h, render } from "preact";
-import { App } from "./App/App";
-
-const app = document.createElement("div");
-app.id = "app";
-document.body.appendChild(app);
-
-render(<App />, app);
