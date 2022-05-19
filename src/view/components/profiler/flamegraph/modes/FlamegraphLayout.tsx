1fe8a113545dbb2263a48eee818e62fc10fd7a58
Marvin Hagemeister <hello@marvinh.dev>
2022-05-19 06:03:21 +0200

    WIP

--- src/view/components/profiler/flamegraph/modes/FlamegraphLayout.tsx
+++ src/view/components/profiler/flamegraph/modes/FlamegraphLayout.tsx
@@ -2,11 +2,12 @@ import { h, Fragment } from "preact";
 import { CommitData } from "../../data/commits";
 import { ID, DevNode } from "../../../../store/types";
 import { FlameNode } from "../FlameNode";
-import { useMemo } from "preact/hooks";
+import { useEffect, useMemo, useRef } from "preact/hooks";
 import { placeFlamegraph } from "./flamegraph-utils";
 import { formatTime } from "../../util";
 import { useObserver, useStore } from "../../../../store/react-bindings";
 import { HocLabels } from "../../../elements/TreeView";
+import s from "../FlameGraph.module.css";
 
 export interface FlamegraphLayoutProps {
 	commit: CommitData;
@@ -16,6 +17,22 @@ export interface FlamegraphLayoutProps {
 	onMouseEnter: (id: ID) => void;
 	onMouseLeave: () => void;
 }
+
+type StyleCache = {
+	commitParent: string;
+	gradient: [
+		string,
+		string,
+		string,
+		string,
+		string,
+		string,
+		string,
+		string,
+		string,
+		string,
+	];
+};
 
 export function FlamegraphLayout({
 	commit,
@@ -41,8 +58,90 @@ export function FlamegraphLayout({
 		[commit, data, selected, canvasWidth],
 	);
 
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const styleCacheRef = useRef<StyleCache>({
+		commitParent: "",
+		gradient: ["", "", "", "", "", "", "", "", "", ""],
+	});
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		if (!canvas) return;
+
+		const styles = window.getComputedStyle(canvas);
+
+		styleCacheRef.current = {
+			commitParent: styles.getPropertyValue("--color-profiler-old"),
+			gradient: [
+				styles.getPropertyValue("--color-profiler-gradient-0"),
+				styles.getPropertyValue("--color-profiler-gradient-1"),
+				styles.getPropertyValue("--color-profiler-gradient-2"),
+				styles.getPropertyValue("--color-profiler-gradient-3"),
+				styles.getPropertyValue("--color-profiler-gradient-4"),
+				styles.getPropertyValue("--color-profiler-gradient-5"),
+				styles.getPropertyValue("--color-profiler-gradient-6"),
+				styles.getPropertyValue("--color-profiler-gradient-7"),
+				styles.getPropertyValue("--color-profiler-gradient-8"),
+				styles.getPropertyValue("--color-profiler-gradient-9"),
+			],
+		};
+	}, []);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		if (!canvas) return;
+
+		canvas.getContext("2d");
+		const ctx = canvas.getContext("2d");
+		if (!ctx) return;
+
+		const w = canvasWidth;
+		const h = 260;
+		const ratio = window.devicePixelRatio;
+		canvas.width = w * ratio;
+		canvas.height = h * ratio;
+		canvas.style.width = w + "px";
+		canvas.style.height = h + "px";
+		ctx.scale(ratio, ratio);
+		// ctx.imageSmoothingEnabled = false;
+		// ctx.translate(0.5, 0.5);
+
+		const styles = styleCacheRef.current;
+
+		placed.forEach((pos, i) => {
+			if (!pos.visible) return;
+
+			if (pos.weight > -1) {
+				ctx.fillStyle = styles.gradient[pos.weight];
+			} else if (pos.commitParent) {
+				ctx.fillStyle = styles.commitParent;
+			} else {
+				ctx.fillStyle = "red";
+			}
+
+			const ROW_HEIGHT = 21; // Account 1px for border
+			ctx.fillRect(pos.x, pos.row * (ROW_HEIGHT + 1), pos.width, ROW_HEIGHT);
+
+			if (true) {
+				const node = commit.nodes.get(pos.id);
+				if (!node) return;
+
+				ctx.fillStyle = "black";
+				ctx.font = "400 10pt Arial";
+				ctx.fillText(node.name, pos.x + 6, pos.row * ROW_HEIGHT + 16);
+			}
+		});
+
+		console.log(canvas);
+	}, []);
+
 	return (
-		<Fragment>
+		<div>
+			<canvas
+				ref={canvasRef}
+				class={s.flameCanvas}
+				width={canvasWidth}
+				height="260"
+			/>
 			{placed.map(pos => {
 				const node = commit.nodes.get(pos.id)!;
 				let appendix = "";
@@ -72,6 +171,6 @@ export function FlamegraphLayout({
 					</FlameNode>
 				);
 			})}
-		</Fragment>
+		</div>
 	);
 }
