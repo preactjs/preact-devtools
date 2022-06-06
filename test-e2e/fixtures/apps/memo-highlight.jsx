ebd15a2fd3bfd25a233fd7de3132c685c353dd08
Marvin Hagemeister <hello@marvinh.dev>
2022-06-06 19:25:36 +0200

    Add memo highlight test fixture

---
+++ test-e2e/fixtures/apps/memo-highlight.jsx
@@ -0,0 +1,47 @@
+import { h, render } from "preact";
+import { useState } from "preact/hooks";
+import { memo } from "preact/compat";
+
+export const Result = ({ result }) => {
+	return (
+		<div class="result">
+			<Nested text={result} />
+		</div>
+	);
+};
+
+const Nested = ({ text }) => {
+	return <p>{text}</p>;
+};
+
+const MemoResult = memo(Result, () => true);
+
+const generateFakeData = () => {
+	const rand = () => Math.random() * 10;
+	return new Array(3).fill(rand());
+};
+
+const App = () => {
+	const [results, setResults] = useState(generateFakeData());
+
+	return (
+		<div>
+			<h1>Example</h1>
+			<button onClick={() => setResults(generateFakeData())}>Refresh</button>
+			<div class="list">
+				<h2>No memo</h2>
+				{results.map((result, i) => (
+					<Result key={i} result={result} />
+				))}
+			</div>
+			<div class="list">
+				<h2>Memo</h2>
+				{results.map((result, i) => (
+					<MemoResult key={i} result={result} />
+				))}
+			</div>
+		</div>
+	);
+};
+
+render(<App />, document.getElementById("app"));
