f50369f1f244615075ae139002af13c984cf6b39
Marvin Hagemeister <hello@marvinh.dev>
2022-07-09 20:33:30 +0200

    SidebarLayout: Convert CSS Modules to plain CSS

--- src/view/components/SidebarLayout.tsx
+++ src/view/components/SidebarLayout.tsx
@@ -1,22 +1,21 @@
 import { h } from "preact";
-import s from "./SidebarLayout.module.css";
 
 export interface ChildProps {
 	children: any;
 }
 
 export function SidebarLayout(props: ChildProps) {
-	return <div class={s.root}>{props.children}</div>;
+	return <div class="sidebar-layout">{props.children}</div>;
 }
 
 export function SingleLayout(props: ChildProps) {
-	return <div class={s.rootSingle}>{props.children}</div>;
+	return <div class="sidebar-layout-single">{props.children}</div>;
 }
 
 export function PageLayout(props: ChildProps) {
 	return (
-		<div class={s.rootPage}>
-			<div class={s.rootPageInner}>{props.children}</div>
+		<div class="sidebar-layout-root">
+			<div class="sidebar-layout-inner">{props.children}</div>
 		</div>
 	);
 }
