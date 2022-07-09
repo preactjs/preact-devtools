40179f2b5e42ba723d1bc733d888c6f0fe97b465
Marvin Hagemeister <hello@marvinh.dev>
2022-07-09 19:52:42 +0200

    SidebarPanel: Convert CSS Modules to plain CSS

--- src/view/components/sidebar/SidebarPanel.tsx
+++ src/view/components/sidebar/SidebarPanel.tsx
@@ -1,5 +1,4 @@
 import { h, ComponentChildren } from "preact";
-import s from "./SidebarPanel.module.css";
 import { IconBtn } from "../IconBtn";
 import { FileCopy } from "../icons";
 
@@ -12,20 +11,20 @@ export interface Props {
 
 export function SidebarPanel(props: Props) {
 	return (
-		<div class={s.panel} data-testid={props.testId}>
-			<header class={s.panelHeader}>
-				<h3 class={s.title}>{props.title}</h3>
+		<div class="sidebar-panel" data-testid={props.testId}>
+			<header class="sidebar-panel-header">
+				<h3 class="sidebar-panel-title">{props.title}</h3>
 				{props.onCopy && props.children != null && (
 					<IconBtn onClick={props.onCopy} title={`Copy ${props.title}`}>
 						<FileCopy />
 					</IconBtn>
 				)}
 			</header>
-			<div class={s.content}>{props.children}</div>
+			<div class="sidebar-panel-content">{props.children}</div>
 		</div>
 	);
 }
 
 export function Empty(props: { children?: any }) {
-	return <span class={s.empty}>{props.children}</span>;
+	return <span class="sidebar-panel-placeholder">{props.children}</span>;
 }
