6525c3343d9ab56178be9ff2cb332db5090bdcce
Marvin Hagemeister <hello@marvinh.dev>
2023-03-06 18:19:19 +0100

    Update run:BROWSER commands to use the debug build

--- tools/run-chrome.js
+++ tools/run-chrome.js
@@ -3,7 +3,7 @@ const { chromium } = require("playwright-chromium");
 const path = require("path");
 
 async function main() {
-	const extension = path.join(__dirname, "..", "dist", "chrome");
+	const extension = path.join(__dirname, "..", "dist", "chrome-debug");
 	const browser = await chromium.launchPersistentContext("./profiles/chrome", {
 		args: [
 			`--disable-extensions-except=${extension}`,
