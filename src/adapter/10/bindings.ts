b12ee9bc79ca20a3e9b044e135e5b2014220cf64
Jovi De Croock <decroockjovi@gmail.com>
2025-01-15 09:45:49 +0100

    Check for presence of __l (#495)
    If a defaultValue is specified we would erroneously take that as the contextRef rather than the actual ref

--- src/adapter/10/bindings.ts
+++ src/adapter/10/bindings.ts
@@ -212,7 +212,7 @@ export function getDisplayName(vnode: VNode, config: RendererConfig): string {
 
 			// Provider
 			if ((c as any).sub) {
-				const ctx = (type as any)._contextRef || (type as any).__ || (type as any).__l;
+				const ctx = (type as any)._contextRef || ('__l' in type ? (type as any).__l : (type as any).__);
 				if (ctx && ctx.displayName) {
 					return `${ctx.displayName}.Provider`;
 				}
