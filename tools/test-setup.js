6c6205571b306c6522580ce1391fdb0144636617
Marvin Hagemeister <hello@marvinh.dev>
2025-03-24 10:56:34 +0100

    chore: switch mochette -> mocha

--- tools/test-setup.js
+++ tools/test-setup.js
@@ -1,1 +1,20 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+const { JSDOM } = require("jsdom");
+const { performance } = require("perf_hooks");
+
+const dom = new JSDOM(`
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body></body>
+</html>
+`);
+
+global.expect = expect;
+global.sinon = sinon;
+global.document = dom.window.document;
+global.window = dom.window;
+global.performance = performance;
+
 global.__DEBUG__ = false;
